a605d465a668bc8407f3cbed5205aac01ae86b68
Yash <yashdeepsingh@hotmail.com>
2019-12-10 16:13:34 +0530

    Added more Pug pages

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,3 @@
-export const join = (req, res) => res.send("join");
-export const login = (req, res) => res.send("login");
+export const join = (req, res) => res.render("join");
+export const login = (req, res) => res.render("login");
 export const logout = (req, res) => res.send("logout");

27e823eb9dbf2fbad18ed57c90152fb9d03e7860
Yash <yashdeepsingh@hotmail.com>
2019-12-10 21:33:27 +0530

    some edits

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
 export const join = (req, res) => res.render("join");
 export const login = (req, res) => res.render("login");
 export const logout = (req, res) => res.send("logout");
+
+export const editProfile = (req, res) => res.render("editProfile");
---
+++ db.js
@@ -0,0 +1,41 @@
+export const videos = [
+    {
+        id: 324393,
+        title: "Video awesome1",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            id: 121212,
+            name: "Nicolas",
+            email: "nico@gma.com"
+        }
+
+    },
+    {
+        id: 324373,
+        title: "Video Perfect",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            id: 121212,
+            name: "Nicolas",
+            email: "nico@gma.com"
+        }
+
+    },
+    {
+        id: 324383,
+        title: "Video Nice",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            id: 121212,
+            name: "Nicolas",
+            email: "nico@gma.com"
+        }
+
+    }
+]

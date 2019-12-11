353d0e90ef26e0d4aff11ffcd7ab73bdc12d68bc
Yash <yashdeepsingh@hotmail.com>
2019-12-11 17:05:49 +0530

    Added MongoDb with model(In Progress) and added some in views

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import routes from '../routes';
-import { editProfile } from '../controllers/userController';
+import { editProfile, userDetail } from '../controllers/userController';
 
 const userRouter = express.Router();
 
 userRouter.get(routes.editProfile, editProfile)
+userRouter.get(routes.userDetail(), userDetail)
 
 export default userRouter;

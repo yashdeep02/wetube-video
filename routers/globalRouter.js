7f01f27ff3f9aabf6ec1e9115c1cefcfda804c99
Yash <yashdeepsingh@hotmail.com>
2019-12-13 12:38:41 +0530

    Added Github authentication and userdetail page @me page

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
-import { login, logout, getJoin, postJoin, getLogin, postLogin } from "../controllers/userController";
+import { login, logout, getJoin, postJoin, getLogin, postLogin, githubLogin, postGithubLogin, getMe } from "../controllers/userController";
 import { onlyPublic } from "../middlewares";
+import passport from "passport"
 
 const globalRouter = express.Router();
 
@@ -13,5 +14,11 @@ globalRouter.post(routes.join,onlyPublic, postJoin, postLogin);
 globalRouter.get(routes.login,onlyPublic, getLogin);
 globalRouter.post(routes.login, onlyPublic,postLogin);
 globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.me, getMe)
+globalRouter.get(routes.github, githubLogin);
+globalRouter.get(routes.githubCallback, passport.authenticate("github" ,{ 
+    failureRedirect: "/login"
+}
+),postGithubLogin)
 
 export default globalRouter;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import routes from '../routes';
-import { editProfile, userDetail } from '../controllers/userController';
+import { editProfile, userDetail, me, getMe } from '../controllers/userController';
 import { onlyPrivate } from '../middlewares';
 
 const userRouter = express.Router();
 
 userRouter.get(routes.editProfile, onlyPrivate, editProfile)
-userRouter.get(routes.userDetail(), onlyPrivate, userDetail)
+userRouter.get(routes.userDetail, onlyPrivate, getMe)
 
 export default userRouter;

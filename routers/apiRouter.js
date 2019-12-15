03ef0a5837d6a008b91bc1273359acd3daefdb2b
Yash <yashdeepsingh@hotmail.com>
2019-12-15 17:39:00 +0530

    Added Mongo Atlas and S3 connectivity with end features

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import routes from "../routes";
 import { uploadVideo, onlyPrivate } from "../middlewares";
-import { postRegisterView } from "../controllers/videoController";
+import { postRegisterView, postAddComment } from "../controllers/videoController";
 
 const apiRouter = express.Router();
 
 apiRouter.post(routes.registerView,postRegisterView)
+apiRouter.post(routes.addComment, postAddComment)
 
 export default apiRouter;
